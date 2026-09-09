c63aa78313674b5b747b8e36adf141c06c5c6ab9
William <paez.william8@gmail.com>
2026-09-08 20:56:05 -0400

    fix(module kavac): Url sin funcionar de momento

--- resources/js/components/Admins/AuditRecordsComponent.vue
+++ resources/js/components/Admins/AuditRecordsComponent.vue
@@ -393,8 +393,6 @@ export default {
             'users': 'col-md-2',
             'id': 'col-md-1'
         };
-
-        // Tabla forzada a usar post
         vm.table_options.requestFunction = function(data) {
             return axios.post(window.app_url.replace(/\/$/, "") + '/app/audit-records', {
                 query: {
@@ -407,16 +405,11 @@ export default {
                 ascending: data.ascending,
                 page: data.page,
                 orderBy: data.orderBy
-            })
-            .then(response => {
-                // vue-tables-2 necesita un objeto con un array 'data' y un número 'count'
-                return {
-                    data: response.data.data,
-                    count: response.data.count
-                };
-            })
-            .catch(error => {
-                if (error.response && error.response.status == 422 && !error.response.data.result) {
+            }).catch(error => {
+                if (
+                    !error.response.data.result &&
+                    error.response.status == 422
+                ) {
                     vm.showMessage(
                         "custom",
                         "Error",
@@ -424,15 +417,8 @@ export default {
                         "screen-error",
                         error.response.data.message
                     );
-                } else {
-                    console.error(error);
                 }
-                
-                // Retornar un arreglo vacío y conteo 0 en caso de error para que la tabla no se rompa
-                return {
-                    data: [],
-                    count: 0
-                };
+                console.error(error);
             });
         };
 
